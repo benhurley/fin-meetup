bc1812aed32399d7a5e7477f850229cc79aeae69
Daniel Enciso <danenciso@gmail.com>
2019-06-11 10:41:13 -0500

    [Update] Add styling and correct dimensions for Group cards.

--- fm-app/src/app/home/home.component.ts
+++ fm-app/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
 import { Component } from '@angular/core';
 
-@Component({ templateUrl: 'home.component.html' })
+@Component({ templateUrl: 'home.component.html',styleUrls: ['./home.component.css'] })
 export class HomeComponent {}
