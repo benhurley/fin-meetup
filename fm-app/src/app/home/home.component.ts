85b97751829b91abc9f79ff6f0b909ec69e797d8
Daniel Enciso <danenciso@gmail.com>
2019-06-13 11:13:16 -0500

    [update] Begin modifying home

--- fm-app/src/app/home/home.component.ts
+++ fm-app/src/app/home/home.component.ts
@@ -1,4 +1,17 @@
 import { Component } from '@angular/core';
+import { groups } from '../staticdatafiles/allgroups';
+import { events } from '../staticdatafiles/allevents';
+import { users } from '../staticdatafiles/allusers';
+import {Router} from '@angular/router';
 
-@Component({ templateUrl: 'home.component.html', styleUrls: ['./home.component.css'] })
-export class HomeComponent {}
+@Component({ 
+    templateUrl: 'home.component.html', 
+    styleUrls: ['./home.component.css'] 
+})
+
+export class HomeComponent {
+    groups = groups;
+    events = events;
+    users = users;
+    // create new group list based off of user membership.
+}
