d6f2dc5a665fc0f648f8caa6f2351da7660fc757
Daniel Enciso <danenciso@gmail.com>
2019-06-13 10:57:30 -0500

    [update] Add join event functionality

--- fm-app/src/app/joinevent/joinevent.component.ts
+++ fm-app/src/app/joinevent/joinevent.component.ts
@@ -1,4 +1,26 @@
 import { Component } from '@angular/core';
+import { events } from '../staticdatafiles/allevents';
+import { users } from '../staticdatafiles/allusers';
+import { Router } from '@angular/router';
 
-@Component({ templateUrl: 'joinevent.component.html',styleUrls: ['./joinevent.component.css'] })
-export class JoineventComponent {}
+@Component({
+    templateUrl: 'joinevent.component.html',
+    styleUrls: ['./joinevent.component.css']
+})
+
+export class JoineventComponent {
+    constructor(private router: Router) {
+
+    }
+    // Iterable list
+    events = events;
+    users = users;
+
+    joinEvent(event, eventid, user, userid) {
+        console.log("Joining this event: " + eventid + " " + event + " for user: " + userid + " " + user);
+        // search through groups and check if user already belongs
+
+        // then route to home
+        this.router.navigate(["/"]);
+    }
+}
