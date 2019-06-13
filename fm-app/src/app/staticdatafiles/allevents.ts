0e55d75d1545009c84bf30bb5d21931183eb9ab8
Daniel Enciso <danenciso@gmail.com>
2019-06-13 09:32:55 -0500

    [update] Include static data files

---
+++ fm-app/src/app/staticdatafiles/allevents.ts
@@ -0,0 +1,220 @@
+// events with appropriate group
+
+export var events = [
+    {
+        eventName: "AWS Event 1",
+        imgUrl: "https://www.logolynx.com/images/logolynx/7f/7f0b42ea89979e09c58b53a13e4565d9.jpeg",
+        dateTime: "",
+        location: "WeWork Downtown",
+        eventDetails: "Random event 1",
+        eventId: 1,
+        groupId: 1,
+        attendees: [
+            1, 2, 3, 4
+        ]
+    },
+    {
+        eventName: "AWS Event 2",
+        imgUrl: "https://www.logolynx.com/images/logolynx/7f/7f0b42ea89979e09c58b53a13e4565d9.jpeg",
+        dateTime: "",
+        location: "WeWork Uptown",
+        eventDetails: "Random event 2",
+        eventId: 2,
+        groupId: 1,
+        attendees: [
+            1, 2, 3, 4
+        ]
+    },
+    {
+        eventName: "AWS Event 3",
+        imgUrl: "https://www.logolynx.com/images/logolynx/7f/7f0b42ea89979e09c58b53a13e4565d9.jpeg",
+        dateTime: "",
+        location: "WeWork Midtown",
+        eventDetails: "Random event 3",
+        eventId: 3,
+        groupId: 1,
+        attendees: [
+            1, 2, 3, 4
+        ]
+    },
+    {
+        eventName: "Azure Event 1",
+        imgUrl: "https://www.logolynx.com/images/logolynx/7f/7f0b42ea89979e09c58b53a13e4565d9.jpeg",
+        dateTime: "",
+        location: "Venice Beach",
+        eventDetails: "Random event 4",
+        eventId: 4,
+        groupId: 2,
+        attendees: [
+            1, 2, 3, 4
+        ]
+    },
+    {
+        eventName: "Azure Event 2",
+        imgUrl: "https://www.logolynx.com/images/logolynx/7f/7f0b42ea89979e09c58b53a13e4565d9.jpeg",
+        dateTime: "",
+        location: "PCH",
+        eventDetails: "Random event 5",
+        eventId: 5,
+        groupId: 2,
+        attendees: [
+            1, 2, 3, 4
+        ]
+    },
+    {
+        eventName: "Azure Event 3",
+        imgUrl: "https://www.logolynx.com/images/logolynx/7f/7f0b42ea89979e09c58b53a13e4565d9.jpeg",
+        dateTime: "",
+        location: "Staples Center",
+        eventDetails: "Random event 6",
+        eventId: 6,
+        groupId: 2,
+        attendees: [
+            1, 2, 3, 4
+        ]
+    },
+    {
+        eventName: "Bootstrap Event 1",
+        imgUrl: "https://www.logolynx.com/images/logolynx/7f/7f0b42ea89979e09c58b53a13e4565d9.jpeg",
+        dateTime: "",
+        location: "WeWork East",
+        eventDetails: "Random event 8",
+        eventId: 7,
+        groupId: 3,
+        attendees: [
+            5, 6, 7, 8
+        ]
+    },
+    {
+        eventName: "Bootstrap Event 2",
+        imgUrl: "https://www.logolynx.com/images/logolynx/7f/7f0b42ea89979e09c58b53a13e4565d9.jpeg",
+        dateTime: "",
+        location: "WeWork West",
+        eventDetails: "Random event 9",
+        eventId: 8,
+        groupId: 3,
+        attendees: [
+            5, 6, 7, 8
+        ]
+    },
+    {
+        eventName: "Bootstrap Event 3",
+        imgUrl: "https://www.logolynx.com/images/logolynx/7f/7f0b42ea89979e09c58b53a13e4565d9.jpeg",
+        dateTime: "",
+        location: "WeWork North",
+        eventDetails: "Random event 10",
+        eventId: 9,
+        groupId: 3,
+        attendees: [
+            5, 6, 7, 8
+        ]
+    },
+    {
+        eventName: "Microsoft Event 1",
+        imgUrl: "https://www.logolynx.com/images/logolynx/7f/7f0b42ea89979e09c58b53a13e4565d9.jpeg",
+        dateTime: "",
+        location: "Space Needle",
+        eventDetails: "Random event 10",
+        eventId: 10,
+        groupId: 4,
+        attendees: [
+            5, 6, 7, 8
+        ]
+    },
+    {
+        eventName: "Microsoft Event 2",
+        imgUrl: "https://www.logolynx.com/images/logolynx/7f/7f0b42ea89979e09c58b53a13e4565d9.jpeg",
+        dateTime: "",
+        location: "Microsoft Campus",
+        eventDetails: "Random event 11",
+        eventId: 11,
+        groupId: 4,
+        attendees: [
+            5, 6, 7, 8
+        ]
+    },
+    {
+        eventName: "Microsoft Event 3",
+        imgUrl: "https://www.logolynx.com/images/logolynx/7f/7f0b42ea89979e09c58b53a13e4565d9.jpeg",
+        dateTime: "",
+        location: "WeWork Central",
+        eventDetails: "Random event 12",
+        eventId: 12,
+        groupId: 4,
+        attendees: [
+            5, 6, 7, 8
+        ]
+    },
+    {
+        eventName: "Apple Event 1",
+        imgUrl: "https://www.logolynx.com/images/logolynx/7f/7f0b42ea89979e09c58b53a13e4565d9.jpeg",
+        dateTime: "",
+        location: "HQ",
+        eventDetails: "Random event 13",
+        eventId: 13,
+        groupId: 5,
+        attendees: [
+            1, 2, 3, 4
+        ]
+    },
+    {
+        eventName: "Apple Event 2",
+        imgUrl: "https://www.logolynx.com/images/logolynx/7f/7f0b42ea89979e09c58b53a13e4565d9.jpeg",
+        dateTime: "",
+        location: "Downtown",
+        eventDetails: "Random event 14",
+        eventId: 14,
+        groupId: 5,
+        attendees: [
+            1, 2, 3, 4
+        ]
+    },
+    {
+        eventName: "Apple Event 3",
+        imgUrl: "https://www.logolynx.com/images/logolynx/7f/7f0b42ea89979e09c58b53a13e4565d9.jpeg",
+        dateTime: "",
+        location: "City",
+        eventDetails: "Random event 15",
+        eventId: 15,
+        groupId: 5,
+        attendees: [
+            1, 2, 3, 4
+        ]
+    },
+    {
+        eventName: "Tesla Event 1",
+        imgUrl: "https://www.logolynx.com/images/logolynx/7f/7f0b42ea89979e09c58b53a13e4565d9.jpeg",
+        dateTime: "",
+        location: "There",
+        eventDetails: "Random event 16",
+        eventId: 16,
+        groupId: 6,
+        attendees: [
+            1, 2, 3, 4
+        ]
+    },
+    {
+        eventName: "Tesla Event 2",
+        imgUrl: "https://www.logolynx.com/images/logolynx/7f/7f0b42ea89979e09c58b53a13e4565d9.jpeg",
+        dateTime: "",
+        location: "Here",
+        eventDetails: "Random event 17",
+        eventId: 17,
+        groupId: 6,
+        attendees: [
+            1, 2, 3, 4
+        ]
+    },
+    {
+        eventName: "Tesla Event 3",
+        imgUrl: "https://www.logolynx.com/images/logolynx/7f/7f0b42ea89979e09c58b53a13e4565d9.jpeg",
+        dateTime: "",
+        location: "Somewhere",
+        eventDetails: "Random event 19",
+        eventId: 18,
+        groupId: 6,
+        attendees: [
+            1, 2, 3, 4
+        ]
+    }
+]
---
+++ fm-app/src/app/staticdatafiles/allusers.ts
@@ -0,0 +1,98 @@
+export var users = [
+    {
+        userName: "Dan",
+        userEmail: "dan@test.com",
+        pass: "hashed password",
+        userId: 1,
+        groupMemberships: [
+            1, 2, 5, 6
+        ],
+        eventAttendance: [
+            1, 2, 3, 4, 5, 6, 13, 14, 15, 16, 17, 18
+        ],
+    },
+    {
+        userName: "Ben",
+        userEmail: "best@test.com",
+        pass: "hashed password",
+        userId: 2,
+        groupMemberships: [
+            1, 2, 5, 6
+        ],
+        eventAttendance: [
+            1, 2, 3, 4, 5, 6, 13, 14, 15, 16, 17, 18
+        ],
+    },
+    {
+        userName: "Alex",
+        userEmail: "alex@test.com",
+        pass: "hashed password",
+        userId: 3,
+        groupMemberships: [
+            1, 2, 5, 6
+        ],
+        eventAttendance: [
+            1, 2, 3, 4, 5, 6, 13, 14, 15, 16, 17, 18
+        ],
+    },
+    {
+        userName: "Ed",
+        userEmail: "ed@test.com",
+        pass: "hashed password",
+        userId: 4,
+        groupMemberships: [
+            1, 2, 5, 6
+        ],
+        eventAttendance: [
+            1, 2, 3, 4, 5, 6, 13, 14, 15, 16, 17, 18
+        ],
+    },
+    {
+        userName: "Jen",
+        userEmail: "jen@test.com",
+        pass: "hashed password",
+        userId: 5,
+        groupMemberships: [
+            3, 4
+        ],
+        eventAttendance: [
+            7, 8, 9, 10, 11, 12
+        ],
+    },
+    {
+        userName: "Tanner",
+        userEmail: "tanner@test.com",
+        pass: "hashed password",
+        userId: 6,
+        groupMemberships: [
+            3, 4
+        ],
+        eventAttendance: [
+            7, 8, 9, 10, 11, 12
+        ],
+    },
+    {
+        userName: "Matt",
+        userEmail: "matt@test.com",
+        pass: "hashed password",
+        userId: 7,
+        groupMemberships: [
+            3, 4
+        ],
+        eventAttendance: [
+            7, 8, 9, 10, 11, 12
+        ],
+    },
+    {
+        userName: "Chris",
+        userEmail: "chris@test.com",
+        pass: "hashed password",
+        userId: 8,
+        groupMemberships: [
+            3, 4
+        ],
+        eventAttendance: [
+            7, 8, 9, 10, 11, 12
+        ],
+    }
+]
